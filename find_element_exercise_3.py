76390d56b6ffeaf38a1737c604508ebdb9f484eb
aleksandrSokolovTechEdit <aleksandr.sokolov@tripleten.com>
2024-10-24 14:06:37 +0200

    Fixed find_element_exercise_3.py
    add 
    - time.sleep
    - comments

--- find_element_exercise_3.py
+++ find_element_exercise_3.py
@@ -1,17 +1,24 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Initialize a new instance of the Chrome WebDriver
 driver = webdriver.Chrome()
 
+# Open the specified URL in the browser
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=en")
 
-# Find elements
+# Pause execution for 2 seconds to allow the page to load fully
+time.sleep(2)
+
+# Find the email input field and password input field using their IDs
 email = driver...
 password = driver...
 
-# Test the placeholder attribute for each element
+# Test the placeholder attribute for each input field to ensure they display the correct text
 assert ...
 assert ...
 
-# Close the browser
+# Close the browser and end the WebDriver session
 driver...
