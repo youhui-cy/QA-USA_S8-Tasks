514e0017e73b9f7afcddcc01068b161205b3bd47
aleksandrSokolovTechEdit <aleksandr.sokolov@tripleten.com>
2024-10-24 13:57:40 +0200

    Fixed find_element_exercise_2.py
    add
    - time.sleep
    - comments

--- find_element_exercise_2.py
+++ find_element_exercise_2.py
@@ -1,15 +1,22 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Initialize a new instance of the Chrome WebDriver
 driver = webdriver.Chrome()
 
+# Open the specified URL in the browser
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=en")
 
-# Find all elements
+# Pause execution for 2 seconds to allow the page to load fully
+time.sleep(2)
+
+# Find all image elements on the page using an XPath selector
 elements = driver...
 
 # Check that the number of elements found is greater than 1 by using len()
 ...
 
-# Close the browser
+# Close the browser and end the WebDriver session
 driver...
