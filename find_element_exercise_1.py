4b0ac6a4484589416d13fee4de9704f4139c6ad1
aleksandrSokolovTechEdit <aleksandr.sokolov@tripleten.com>
2024-10-24 13:43:51 +0200

    Fixed find_element_exercise_1.py
    Add 
    - time.sleep
    - comments

--- find_element_exercise_1.py
+++ find_element_exercise_1.py
@@ -1,12 +1,19 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
+# Initialize a new instance of the Chrome WebDriver
 driver = webdriver.Chrome()
 
+# Open the specified URL in the browser
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=en")
 
-# Find the title
+# Pause execution for 2 seconds to allow the page to load fully
+time.sleep(2)
+
+# Find the title element using its CSS Selector
 driver...
 
-# Close the browser
+# Close the browser and end the WebDriver session
 driver...
