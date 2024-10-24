d1f074dd289af9cc653ab8c6764c9e0982f10004
aleksandrSokolovTechEdit <aleksandr.sokolov@tripleten.com>
2024-10-24 16:12:36 +0200

    Fixed get_text.py
    added
    - lots of things

--- get_text.py
+++ get_text.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -5,6 +7,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=en")
+
+time.sleep(2)
 
 # Find the Email field and fill it in
 ...
@@ -18,7 +22,7 @@ driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=en")
 # Add an explicit wait for the page to load
 WebDriverWait(...).until(...)
 
-# Find the button, retrieve its text, and check that the text value is 'Logout'
+# Find the button, retrieve its text, and check that the text value is 'Log out'
 assert ...
 
 driver.quit()
